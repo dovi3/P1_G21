7e1572086e6ebbcd0131f31efefdb7c077efc124
gerardarditcastells <gerardarditcastells@S5-23>
2017-03-21 16:44:44 +0100

    Creant Classe Player

---
+++ CoinArray/CoinRace.cc
@@ -0,0 +1,35 @@
+#include "CoinRace.hh"
+
+// Constructor
+player::player() {
+
+	PlayerPosX = rand() % 10;
+	PlayerPosX = rand() % 10;
+
+}
+
+void player::InitPlayer() {
+
+
+
+}
+void player::movimiento() {
+
+
+ }
+
+void player::PlayerUpdate() {
+
+
+
+}
+
+void main() {
+
+	srand(time(NULL));
+
+	player p1;
+
+	
+
+}
--- CoinArray/CoinRace.hh
+++ CoinArray/CoinRace.hh
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <time.h>
 
+using namespace std;
+
+class player
+{
+private:
+	 //Atributos
+	int PlayerPosX;
+	int PlayerPosY;
+
+	int PlayerScore;
+	//METODOS
+
+	void movimiento();
+	void InitPlayer();
+	void PlayerUpdate();
+
+public:
+
+	player(); // Prototip de constructor
+
+
+	
+
+	player();
+	~player();
+};
+
+
+
+player::player()
+{
+}
+
+player::~player()
+{
+}
